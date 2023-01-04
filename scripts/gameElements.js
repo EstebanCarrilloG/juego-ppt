9f90ecc0937cea4674b549b29cc785d2f54e1ad0
Esteban Carrillo <75136217+EstebanCarrilloG@users.noreply.github.com>
2023-01-04 17:25:04 -0500

    Delete scripts directory

--- scripts/gameElements.js
+++
@@ -1,7 +0,0 @@
-export function renderContent(options) {
-  options.forEach((e) => {
-    btnsContainer.innerHTML += `<button class="game-btn" data-type="${e.type}">
-                                     <img src="${e.url}" alt="${e.type}">
-                                </button>`;
-  });
-}
